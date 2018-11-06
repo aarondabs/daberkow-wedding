06abf49df3338f9bddd1c517fbcfce85854d977f
Aaron Daberkow <aarondabs@gmail.com>
2018-11-06 10:25:04 -0600

    Home page image and styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const HomePage = () => (
     <div className="content-container">
-        <h2>Our Story</h2>
-        <p>Do greatest at in learning steepest. Breakfast extremity suffering one who all otherwise suspected. He at no nothing forbade up moments. Wholly uneasy at missed be of pretty whence. John way sir high than law who week. Surrounded prosperous introduced it if is up dispatched. Improved so strictly produced answered elegance is. 
+        <div className="image">
+            <img src={"/images/home_page.jpg"} />
+        </div>
+        <h2 className="page-title">Our Story</h2>
+        <p className="page-body">Do greatest at in learning steepest. Breakfast extremity suffering one who all otherwise suspected. He at no nothing forbade up moments. Wholly uneasy at missed be of pretty whence. John way sir high than law who week. Surrounded prosperous introduced it if is up dispatched. Improved so strictly produced answered elegance is. 
 
         Is at purse tried jokes china ready decay an. Small its shy way had woody downs power. To denoting admitted speaking learning my exercise so in. Procured shutters mr it feelings. To or three offer house begin taken am at. As dissuade cheerful overcame so of friendly he indulged unpacked. Alteration connection to so as collecting me. Difficult in delivered extensive at direction allowance. Alteration put use diminution can considered sentiments interested discretion. An seeing feebly stairs am branch income me unable. 
         
