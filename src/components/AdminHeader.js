e94e43a226ad04150addeef9380cd91c02c00e84
Aaron Daberkow <aarondabs@gmail.com>
2018-11-21 13:01:53 -0600

    Fixed admin header color and cleaned up css colors

--- src/components/AdminHeader.js
+++ src/components/AdminHeader.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) => (
-    <header className="header">
+    <header className="header header--admin">
         <div className="content-container">
             <div className="header__content">
                 <Link className="header__title" to="/admin">
